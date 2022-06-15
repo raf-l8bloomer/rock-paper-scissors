76722df1c7734c83205cd990886a6f51c134ff85
rafael.lumang <lumang.rafael@gmail.com>
2022-06-15 13:25:45 -0700

    Added loop for gamerounds but needs to be adjusted for new input each round.

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const moves = [
-    "rock", 
-    "paper", 
-    "scissors"
+    'rock', 
+    'paper', 
+    'scissors'
 ];
 
 //console.log(`${moves}`);
+
+/* Produces the Computer's random move*/
 
 function computerPlay (arr) {
     let randomMove = Math.floor(Math.random() * arr.length);
@@ -12,3 +14,49 @@ function computerPlay (arr) {
 };
 
 //console.log(computerPlay(moves));
+
+
+
+function game () {
+    for ( let i = 0; i < 5; i++) {
+        playRound (playerSelection, computerSelection);
+       
+    }  alert(`You:${yourWin} Computer:${compWin}`);
+} 
+
+/* function takes moves parameters, capitalizes the inputs to make them case insensitive, and then compares moves to determine winner.
+ROCK > SCISSORS
+SCISSORS > PAPER
+PAPER > ROCK
+*/
+
+function playRound (playerSelection, computerSelection) {
+    playerSelection;
+    let player = playerSelection.toUpperCase();
+    let computer = computerSelection.toUpperCase();
+    if (player === 'ROCK' && computer === 'SCISSORS'){
+        alert(`You win! ${player} beats ${computer}`)
+        yourWin++;
+    } else if (player === 'SCISSORS' && computer === 'PAPER') {
+        alert(`You win! ${player} beats ${computer}`)
+        yourWin++;
+    }  else if (player === 'PAPER' && computer === 'ROCK') {
+        alert(`You win! ${player} beats ${computer}`)
+        yourWin++;
+    } else if (player === computer){
+        alert (`Tie! Refresh and try again`);
+        yourWin++;
+    } else {
+    alert(`You lose :( ${computer} beats ${player}`);
+    compWin++;
+    }
+}
+
+let yourWin = 0;
+let compWin = 0;
+let playerSelection = prompt('Rock, paper, scissors?');
+let computerSelection = computerPlay(moves);
+console.log(game());
+
+
+
