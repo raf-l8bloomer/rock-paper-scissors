fd7e4d7eca5580d558f6fd06d7a434dd85213b96
rafael.lumang <lumang.rafael@gmail.com>
2022-06-16 10:27:12 -0700

    Successfully added the game function for 5 rounds! + history of moves in console + input move for every round successfully + fixed 'tie' points section

--- script.js
+++ script.js
@@ -20,20 +20,26 @@ function computerPlay (arr) {
 function game () {
     for ( let i = 0; i < 5; i++) {
         playRound (playerSelection, computerSelection);
-       
     }  alert(`You:${yourWin} Computer:${compWin}`);
+    if ( yourWin > compWin){
+        alert(`You ate that.`)
+    } else (
+        alert(`Try again, loser. (refresh)`)
+    )
 } 
 
-/* function takes moves parameters, capitalizes the inputs to make them case insensitive, and then compares moves to determine winner.
+/* function takes "moves" parameters, capitalizes the inputs to make them case insensitive, and then compares moves to determine winner.
 ROCK > SCISSORS
 SCISSORS > PAPER
 PAPER > ROCK
 */
 
 function playRound (playerSelection, computerSelection) {
-    playerSelection;
+    playerSelection = prompt('Rock, paper, scissors?');
+    computerSelection = computerPlay(moves);
     let player = playerSelection.toUpperCase();
     let computer = computerSelection.toUpperCase();
+    console.log(`YOU: ${player} VS. COMP: ${computer}`);
     if (player === 'ROCK' && computer === 'SCISSORS'){
         alert(`You win! ${player} beats ${computer}`)
         yourWin++;
@@ -44,8 +50,8 @@ function playRound (playerSelection, computerSelection) {
         alert(`You win! ${player} beats ${computer}`)
         yourWin++;
     } else if (player === computer){
-        alert (`Tie! Refresh and try again`);
-        yourWin++;
+        alert (`Tie!`);
+        
     } else {
     alert(`You lose :( ${computer} beats ${player}`);
     compWin++;
@@ -54,8 +60,8 @@ function playRound (playerSelection, computerSelection) {
 
 let yourWin = 0;
 let compWin = 0;
-let playerSelection = prompt('Rock, paper, scissors?');
-let computerSelection = computerPlay(moves);
+let playerSelection = '';
+let computerSelection = '';
 console.log(game());
 
 
