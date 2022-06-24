cc16784d3400c94aa9b71a79216c9252dda9731f
rafael.lumang <lumang.rafael@gmail.com>
2022-06-23 18:12:53 -0700

    Added buttons and DOM function however needs to play 5 rounds

--- script.js
+++ script.js
@@ -13,21 +13,51 @@ function computerPlay (arr) {
     return arr[randomMove];
 };
 
+// const rock = document.querySelector('#rock');
+// const paper = document.querySelector('#paper');
+// const scissors = document.querySelector('scissors');
+
+// rock.addEventListener ('click', ()=>{})
+
+// Pulls all button tags a
+const buttons = document.querySelectorAll('button');
+
+//takes each button tag pulled 'buttons' and assigns chosen button to the playerSelection
+//and runs playRound function
+
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+            for ( let i = 0; i < 5; i++) {
+                playerSelection = (button.id);
+                playRound(playerSelection, computerSelection)
+            } results.textContent=(`You:${yourWin} Computer:${compWin}`);
+            if ( yourWin > compWin){
+                round.textContent = (`You ate that.`)
+            } else (
+                round.textContent = (`Try again, loser. (refresh)`)
+            )
+
+    })
+})
+
+
+
 //console.log(computerPlay(moves));
 
-/* function allows 5 ounds of playRound function while alerting final message of the winner of the game
+/* function allows 5 rounds of playRound function while alerting final message of the winner of the game
 */
 
-function game () {
-    for ( let i = 0; i < 5; i++) {
-        playRound (playerSelection, computerSelection);
-    }  alert(`You:${yourWin} Computer:${compWin}`);
-    if ( yourWin > compWin){
-        alert(`You ate that.`)
-    } else (
-        alert(`Try again, loser. (refresh)`)
-    )
-} 
+// function game () {
+//     for ( let i = 0; i < 5; i++) {
+//         playRound (playerSelection, computerSelection);
+//     }  alert(`You:${yourWin} Computer:${compWin}`);
+//     if ( yourWin > compWin){
+//         round.textContent = (`You ate that.`)
+//     } else (
+//         round.textContent = (`Try again, loser. (refresh)`)
+//     )
+// } 
 
 /* function takes "moves" parameters, capitalizes the inputs to make them case insensitive, and then compares moves to determine winner.
 ROCK > SCISSORS
@@ -36,25 +66,25 @@ PAPER > ROCK
 */
 
 function playRound (playerSelection, computerSelection) {
-    playerSelection = prompt('Rock, paper, scissors?');
+    // playerSelection = prompt('Rock, paper, scissors?');
     computerSelection = computerPlay(moves);
     let player = playerSelection.toUpperCase();
     let computer = computerSelection.toUpperCase();
     console.log(`YOU: ${player} VS. COMP: ${computer}`);
     if (player === 'ROCK' && computer === 'SCISSORS'){
-        alert(`You win! ${player} beats ${computer}`)
+        round.textContent = (`You win! ${player} beats ${computer}`)
         yourWin++;
     } else if (player === 'SCISSORS' && computer === 'PAPER') {
-        alert(`You win! ${player} beats ${computer}`)
+        round.textContent = (`You win! ${player} beats ${computer}`)
         yourWin++;
     }  else if (player === 'PAPER' && computer === 'ROCK') {
-        alert(`You win! ${player} beats ${computer}`)
+        round.textContent = (`You win! ${player} beats ${computer}`)
         yourWin++;
     } else if (player === computer){
-        alert (`Tie!`);
+        round.textContent =  (`Tie!`);
         
     } else {
-    alert(`You lose :( ${computer} beats ${player}`);
+        round.textContent = (`You lose :( ${computer} beats ${player}`);
     compWin++;
     }
 }
@@ -63,7 +93,19 @@ let yourWin = 0;
 let compWin = 0;
 let playerSelection = '';
 let computerSelection = '';
-console.log(game());
+// console.log(game());
 
+
+const resultDiv = document.createElement('div');
+const body = document.querySelector('body');
+body.appendChild(resultDiv);
+
+const round = document.createElement('p');
+resultDiv.appendChild(round);
+
+
+const results = document.createElement('h3');
+resultDiv.appendChild(results);
+results.textContent = `YOU: ${yourWin} vs COMP: ${compWin}`;
 
 
